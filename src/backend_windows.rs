2d5cf4cb8110413a270ac197c0858b0ac50ee18e
Dana Keeler <dkeeler@mozilla.com>
2019-08-27 13:01:44 -0700

    refactor getting keys on Windows

--- src/backend_windows.rs
+++ src/backend_windows.rs
@@ -30,6 +30,40 @@ pub struct Cert {
 }
 
 impl Cert {
+    fn new(cert: PCCERT_CONTEXT) -> Result<Cert, ()> {
+        let cert = unsafe { &*cert };
+        let cert_info = unsafe { &*cert.pCertInfo };
+        let value =
+            unsafe { slice::from_raw_parts(cert.pbCertEncoded, cert.cbCertEncoded as usize) };
+        let value = value.to_vec();
+        let id = Sha256::digest(&value).to_vec();
+        let label = id.clone(); // TODO
+        let issuer = unsafe {
+            slice::from_raw_parts(cert_info.Issuer.pbData, cert_info.Issuer.cbData as usize)
+        };
+        let issuer = issuer.to_vec();
+        let serial_number = unsafe {
+            slice::from_raw_parts(
+                cert_info.SerialNumber.pbData,
+                cert_info.SerialNumber.cbData as usize,
+            )
+        };
+        let serial_number = serial_number.to_vec();
+        let subject = unsafe {
+            slice::from_raw_parts(cert_info.Subject.pbData, cert_info.Subject.cbData as usize)
+        };
+        let subject = subject.to_vec();
+        Ok(Cert {
+            class: serialize_uint(CKO_CERTIFICATE),
+            token: serialize_uint(CK_TRUE),
+            id,
+            label,
+            value,
+            issuer,
+            serial_number,
+            subject,
+        })
+    }
     fn class(&self) -> &[u8] {
         &self.class
     }
@@ -98,6 +132,77 @@ impl Cert {
     }
 }
 
+struct CertContext(PCCERT_CONTEXT);
+
+// TODO: CERT_CONTEXT may not be thread-safe (unclear) (but in any case we're protected by the mutex
+// on the manager, so this should be ok?)
+unsafe impl Send for CertContext {}
+unsafe impl Sync for CertContext {}
+
+impl CertContext {
+    fn new(cert: PCCERT_CONTEXT) -> CertContext {
+        CertContext(unsafe { CertDuplicateCertificateContext(cert) })
+    }
+}
+
+impl Drop for CertContext {
+    fn drop(&mut self) {
+        unsafe {
+            CertFreeCertificateContext(self.0);
+        }
+    }
+}
+
+impl Deref for CertContext {
+    type Target = PCCERT_CONTEXT;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+struct NCryptKeyHandle(NCRYPT_KEY_HANDLE);
+
+impl NCryptKeyHandle {
+    fn from_cert(cert: &CertContext) -> Result<NCryptKeyHandle, ()> {
+        let mut key_handle = 0;
+        let mut key_spec = 0;
+        let mut must_free = 0;
+        unsafe {
+            if CryptAcquireCertificatePrivateKey(
+                **cert,
+                CRYPT_ACQUIRE_ONLY_NCRYPT_KEY_FLAG, // currently we only support CNG
+                std::ptr::null_mut(),
+                &mut key_handle,
+                &mut key_spec,
+                &mut must_free,
+            ) != 1
+            {
+                return Err(());
+            }
+        }
+        assert!(key_spec == CERT_NCRYPT_KEY_SPEC);
+        assert!(must_free != 0);
+        Ok(NCryptKeyHandle(key_handle as NCRYPT_KEY_HANDLE))
+    }
+}
+
+impl Drop for NCryptKeyHandle {
+    fn drop(&mut self) {
+        unsafe {
+            NCryptFreeObject(self.0 as NCRYPT_HANDLE);
+        }
+    }
+}
+
+impl Deref for NCryptKeyHandle {
+    type Target = NCRYPT_KEY_HANDLE;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 #[derive(Debug)]
 pub enum KeyType {
     EC,
@@ -105,7 +210,7 @@ pub enum KeyType {
 }
 
 pub struct Key {
-    handle: NCRYPT_KEY_HANDLE, // TODO: scope this?
+    cert: CertContext,
     class: Vec<u8>,
     token: Vec<u8>,
     id: Vec<u8>,
@@ -117,6 +222,62 @@ pub struct Key {
 }
 
 impl Key {
+    fn new(cert_context: PCCERT_CONTEXT) -> Result<Key, ()> {
+        let cert = unsafe { *cert_context };
+        let cert_der =
+            unsafe { slice::from_raw_parts(cert.pbCertEncoded, cert.cbCertEncoded as usize) };
+        let id = Sha256::digest(cert_der).to_vec();
+        let id = id.to_vec();
+        let cert_info = unsafe { &*cert.pCertInfo };
+        let mut modulus = None;
+        let mut ec_params = None;
+        let spki = &cert_info.SubjectPublicKeyInfo;
+        let algorithm_oid = unsafe { CStr::from_ptr(spki.Algorithm.pszObjId) }
+            .to_str()
+            .map_err(|_| ())?;
+        let (key_type_enum, key_type_attribute) = if algorithm_oid == szOID_RSA_RSA {
+            if spki.PublicKey.cUnusedBits != 0 {
+                return Err(());
+            }
+            let public_key_bytes = unsafe {
+                std::slice::from_raw_parts(spki.PublicKey.pbData, spki.PublicKey.cbData as usize)
+            };
+            // TODO: this is shared with the MacOS implementation - refactor to der module?
+            // RSAPublicKey ::= SEQUENCE {
+            //     modulus           INTEGER,  -- n
+            //     publicExponent    INTEGER   -- e
+            // }
+            let mut sequence = Sequence::new(public_key_bytes)?;
+            let modulus_value = sequence.read_unsigned_integer()?;
+            let exponent = sequence.read_unsigned_integer()?;
+            if !sequence.at_end() {
+                return Err(());
+            }
+            modulus = Some(modulus_value.to_vec());
+            (KeyType::RSA, CKK_RSA)
+        } else if algorithm_oid == szOID_ECC_PUBLIC_KEY {
+            let params = &spki.Algorithm.Parameters;
+            ec_params = Some(
+                unsafe { std::slice::from_raw_parts(params.pbData, params.cbData as usize) }
+                    .to_vec(),
+            );
+            (KeyType::EC, CKK_EC)
+        } else {
+            return Err(());
+        };
+        Ok(Key {
+            cert: CertContext::new(cert_context),
+            class: serialize_uint(CKO_PRIVATE_KEY),
+            token: serialize_uint(CK_TRUE),
+            id,
+            private: serialize_uint(CK_TRUE),
+            key_type: serialize_uint(key_type_attribute),
+            modulus,
+            ec_params,
+            key_type_enum,
+        })
+    }
+
     fn class(&self) -> &[u8] {
         &self.class
     }
@@ -192,6 +353,7 @@ impl Key {
 
     // The input data is a hash. What algorithm we use depends on the size of the hash.
     pub fn sign(&self, data: &[u8]) -> Result<Vec<u8>, ()> {
+        let key = NCryptKeyHandle::from_cert(&self.cert)?;
         let mut data = data.to_vec();
         let (params, flags) = match self.key_type_enum {
             KeyType::EC => (None, 0),
@@ -213,7 +375,7 @@ impl Key {
         // TODO: len conversion safety
         let status = unsafe {
             NCryptSignHash(
-                self.handle,
+                *key,
                 params_ptr,
                 data.as_mut_ptr(),
                 data.len() as u32,
@@ -234,7 +396,7 @@ impl Key {
         let mut final_signature_len = signature_len;
         let status = unsafe {
             NCryptSignHash(
-                self.handle,
+                *key,
                 params_ptr,
                 data.as_mut_ptr(),
                 data.len() as u32,
@@ -251,14 +413,6 @@ impl Key {
         }
         assert!(final_signature_len == signature_len);
         Ok(signature)
-    }
-}
-
-impl Drop for Key {
-    fn drop(&mut self) {
-        unsafe {
-            NCryptFreeObject(self.handle as NCRYPT_HANDLE);
-        }
     }
 }
 
@@ -281,100 +435,6 @@ impl Object {
             Object::Key(key) => key.get_attribute(attribute),
         }
     }
-}
-
-fn cert_from_cert_context(context: &CERT_CONTEXT) -> Cert {
-    let cert_info = unsafe { &*context.pCertInfo };
-    let value =
-        unsafe { slice::from_raw_parts(context.pbCertEncoded, context.cbCertEncoded as usize) };
-    let value = value.to_vec();
-    let id = Sha256::digest(&value).to_vec();
-    let label = id.clone(); // TODO
-    let issuer =
-        unsafe { slice::from_raw_parts(cert_info.Issuer.pbData, cert_info.Issuer.cbData as usize) };
-    let issuer = issuer.to_vec();
-    let serial_number = unsafe {
-        slice::from_raw_parts(
-            cert_info.SerialNumber.pbData,
-            cert_info.SerialNumber.cbData as usize,
-        )
-    };
-    let serial_number = serial_number.to_vec();
-    let subject = unsafe {
-        slice::from_raw_parts(cert_info.Subject.pbData, cert_info.Subject.cbData as usize)
-    };
-    let subject = subject.to_vec();
-    Cert {
-        class: serialize_uint(CKO_CERTIFICATE),
-        token: serialize_uint(CK_TRUE),
-        id,
-        label,
-        value,
-        issuer,
-        serial_number,
-        subject,
-    }
-}
-
-fn key_from_cert_context_and_key_handle(
-    cert_context: &CERT_CONTEXT,
-    key_handle: NCRYPT_KEY_HANDLE,
-) -> Result<Key, ()> {
-    let cert_der = unsafe {
-        slice::from_raw_parts(
-            cert_context.pbCertEncoded,
-            cert_context.cbCertEncoded as usize,
-        )
-    };
-    let id = Sha256::digest(cert_der).to_vec();
-    let id = id.to_vec();
-    let cert_info = unsafe { &*cert_context.pCertInfo };
-    let mut modulus = None;
-    let mut ec_params = None;
-    let spki = &cert_info.SubjectPublicKeyInfo;
-    let algorithm_oid = unsafe { CStr::from_ptr(spki.Algorithm.pszObjId) }
-        .to_str()
-        .map_err(|_| ())?;
-    let (key_type_enum, key_type_attribute) = if algorithm_oid == szOID_RSA_RSA {
-        if spki.PublicKey.cUnusedBits != 0 {
-            return Err(());
-        }
-        let public_key_bytes = unsafe {
-            std::slice::from_raw_parts(spki.PublicKey.pbData, spki.PublicKey.cbData as usize)
-        };
-        // TODO: this is shared with the MacOS implementation - refactor to der module?
-        // RSAPublicKey ::= SEQUENCE {
-        //     modulus           INTEGER,  -- n
-        //     publicExponent    INTEGER   -- e
-        // }
-        let mut sequence = Sequence::new(public_key_bytes)?;
-        let modulus_value = sequence.read_unsigned_integer()?;
-        let exponent = sequence.read_unsigned_integer()?;
-        if !sequence.at_end() {
-            return Err(());
-        }
-        modulus = Some(modulus_value.to_vec());
-        (KeyType::RSA, CKK_RSA)
-    } else if algorithm_oid == szOID_ECC_PUBLIC_KEY {
-        let params = &spki.Algorithm.Parameters;
-        ec_params = Some(
-            unsafe { std::slice::from_raw_parts(params.pbData, params.cbData as usize) }.to_vec(),
-        );
-        (KeyType::EC, CKK_EC)
-    } else {
-        return Err(());
-    };
-    Ok(Key {
-        handle: key_handle, // TODO
-        class: serialize_uint(CKO_PRIVATE_KEY),
-        token: serialize_uint(CK_TRUE),
-        id,
-        private: serialize_uint(CK_TRUE),
-        key_type: serialize_uint(key_type_attribute),
-        modulus,
-        ec_params,
-        key_type_enum,
-    })
 }
 
 struct CertStore {
@@ -436,32 +496,16 @@ pub fn list_objects() -> Vec<Object> {
             cert_context,
         );
         while !cert_context.is_null() {
-            // TODO: I think I'm using the skid as the PKCS#11 object ID, which might not be good if
-            // there are multiple certs with the same skid. Switch to hash of cert DER?
-            // TODO: refactor common code?
-            let mut key_handle = 0;
-            let mut key_spec = 0;
-            let mut must_free = 0;
-            // TODO: CRYPT_ACQUIRE_SILENT_FLAG may be helpful here.
-            if CryptAcquireCertificatePrivateKey(
-                cert_context,
-                CRYPT_ACQUIRE_ONLY_NCRYPT_KEY_FLAG, // TODO: currently we only support CNG
-                std::ptr::null_mut(),
-                &mut key_handle,
-                &mut key_spec,
-                &mut must_free,
-            ) == 1
-            {
-                assert!(key_spec == CERT_NCRYPT_KEY_SPEC);
-                assert!(must_free != 0);
-                if let Ok(key) = key_from_cert_context_and_key_handle(
-                    &*cert_context,
-                    key_handle as NCRYPT_KEY_HANDLE,
-                ) {
-                    objects.push(Object::Cert(cert_from_cert_context(&*cert_context)));
-                    objects.push(Object::Key(key));
-                }
-            }
+            let cert = match Cert::new(cert_context) {
+                Ok(cert) => cert,
+                Err(()) => continue,
+            };
+            let key = match Key::new(cert_context) {
+                Ok(key) => key,
+                Err(()) => continue,
+            };
+            objects.push(Object::Cert(cert));
+            objects.push(Object::Key(key));
 
             cert_context = CertFindCertificateInStore(
                 *store,
